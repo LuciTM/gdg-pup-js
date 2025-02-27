97e35ce8e4be9124fdea4d7cf71cbbe0e1e9937c
LuciTM <159764500+LuciTM@users.noreply.github.com>
2025-02-27 18:27:13 +0800

    final commit

---
+++ Assignment-Exercises/JS-5_Arrays-and-Loops/script.js
@@ -0,0 +1,15 @@
+let favoriteMovies = [
+    "Percy Jackson",
+    "Balota",
+    "Sing",
+    "Bad Guys",
+    "Fast Furious: Tokyo Drift",
+    "Spiderman"
+] 
+  
+  let index = 0;
+  while (index < favoriteMovies.length) {
+    console.log(`${favoriteMovies[index]}`);
+    index++;
+  }
+  
